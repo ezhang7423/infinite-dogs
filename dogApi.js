d2807911f4bd4776b38f7c418101b33b31c549fc
Eddie Zhang <54998055+ezhang7423@users.noreply.github.com>
2019-10-05 09:54:59 -0700

    attempt at resizing images
    asynchronous javascript is difficult

--- dogApi.js
+++ dogApi.js
@@ -3,6 +3,8 @@ const DOG_URL = "https://dog.ceo/api/breeds/image/random";
 
 const doggos = document.querySelector(".doggos")
 const newDog = document.querySelector(".newDog")
+let lastDog;
+
 
 
 
@@ -19,6 +21,32 @@ promise
     img.alt = 'dogger'
     img.className = 'dogger'
     doggos.appendChild(img);
+    // setTimeout(resizeDog, 1000)
+
 })
 }
+
+// function resizeDog(){
+//     lastDog = document.querySelectorAll('.dogger')
+//     if (lastDog.length > 1){
+//         img = lastDog[lastDog.length-1]
+//         lastDog = lastDog[lastDog.length-2]
+//         heightRatio = (lastDog.height)/(img.height)
+//         console.log('img ratio', img.height/img.width)
+//         img.height = img.height*heightRatio
+//         img.width = img.width*heightRatio
+//         console.log('img ratio', img.height/img.width)
+//     }
+//     else{
+//         img = lastDog[lastDog.length-1]
+//         og = img.height
+//         img.height = document.body.clientHeight/3
+//         img.width *= document.body.clientHeight/og
+//     }
+// }
+
+
 newDog.addEventListener("click", addNewDogger)
+
+
+
